bec93d8f5552389dd3f86b959020b6d720c3ba59
Philipp <philipp-enke@gmx.de>
2017-04-27 19:58:51 +0200

    added XMLImporter to diagram added XMLImporter to diagram added XMLManager to diagram (facade)

---
+++ ReqAnTool/Client/src/xml/XMLManager.java
@@ -0,0 +1,25 @@
+package xml;
+
+import Model_Interfaces.IXMLManager;
+import Model_Interfaces.IRequirementAnalysis;
+
+public class XMLManager
+  implements IXMLManager
+{
+  private XMLImporter importer;
+  private XMLExporter exporter;
+
+  @Override
+  public int exportAnalysis(IRequirementAnalysis analysis, String address)
+  {
+    // TODO Implement this method
+    return 0;
+  }
+
+  @Override
+  public IRequirementAnalysis importAnalysis()
+  {
+    // TODO Implement this method
+    return null;
+  }
+}
