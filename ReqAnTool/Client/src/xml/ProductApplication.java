d4b1a00e02f8b32eaa214be597fad0d5ea8b06a0
Philipp <philipp-enke@gmx.de>
2017-04-28 19:29:15 +0200

    added Constructor for ProductApplication

--- ReqAnTool/Client/src/xml/ProductApplication.java
+++ ReqAnTool/Client/src/xml/ProductApplication.java
@@ -3,6 +3,11 @@ package xml;
 public class ProductApplication
 {
   private String description;
+  
+  public ProductApplication(String description)
+  {
+    this.description = description;
+  }
 
   public String getDescription()
   {
