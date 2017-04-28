bea99104365442cd03e624850a5d1dd5b16d91ce
Philipp <philipp-enke@gmx.de>
2017-04-28 19:27:45 +0200

    added getter and Constructor for FRequirement

--- ReqAnTool/Client/src/xml/FRequirement.java
+++ ReqAnTool/Client/src/xml/FRequirement.java
@@ -2,8 +2,36 @@ package xml;
 
 public class FRequirement
 {
-  private String iD;
+  private String id;
   private String title;
   private String actor;
   private String description;
+  
+  public FRequirement(String id, String title, String actor, String description)
+  {
+    this.id = id;
+    this.title = title;
+    this.actor = actor;
+    this.description = description;
+  }
+
+  public String getId()
+  {
+    return id;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public String getActor()
+  {
+    return actor;
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
 }
