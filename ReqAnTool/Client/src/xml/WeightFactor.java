7c902e6458bc85d9a4b904b7b4fb10d9fc03e623
Philipp <philipp-enke@gmx.de>
2017-04-28 19:14:55 +0200

    added getter and Constructor for WeightFactor

--- ReqAnTool/Client/src/xml/WeightFactor.java
+++ ReqAnTool/Client/src/xml/WeightFactor.java
@@ -6,4 +6,32 @@ public class WeightFactor
   private int value;
   private int minValue;
   private int maxValue;
+  
+  public WeightFactor(String title, int value, int minValue, int maxValue)
+  {
+    this.title = title;
+    this.value = value;
+    this.minValue = minValue;
+    this.maxValue = maxValue;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public int getValue()
+  {
+    return value;
+  }
+
+  public int getMinValue()
+  {
+    return minValue;
+  }
+
+  public int getMaxValue()
+  {
+    return maxValue;
+  }
 }
