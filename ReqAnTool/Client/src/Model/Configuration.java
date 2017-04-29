2284e92aa0d2c0344c92ca285a3d6bc72e760ab1
Matthias Billharz <Matthias Billharz>
2017-04-29 11:11:32 +0200

    Test

--- ReqAnTool/Client/src/Model/Configuration.java
+++ ReqAnTool/Client/src/Model/Configuration.java
@@ -4,8 +4,23 @@ import java.util.List;
 
 public class Configuration
 {
+    private static ComplexityMatrix myComplexityMatrix;
+    private static ComplexityWeightMatrix myComplexityWeightMatrix;
+
     /**
      * @associates <{Model.WeightFactor}>
      */
     private ArrayList<WeightFactor> optWeightFactor;
+
+    private void getComplexityMatrix()
+    {
+    }
+
+    private void getComplexityWeightMatrix()
+    {
+    }
+
+    public void adjustOptWeightFactors()
+    {
+    }
 }
