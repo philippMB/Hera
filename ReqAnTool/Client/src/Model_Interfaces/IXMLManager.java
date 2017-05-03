0b89ca2fd9d53a74f02f0500896267059360a9ba
Philipp <philipp-enke@gmx.de>
2017-05-03 12:14:19 +0200

    XMLManager updated with Execptions temporary

--- ReqAnTool/Client/src/Model_Interfaces/IXMLManager.java
+++ ReqAnTool/Client/src/Model_Interfaces/IXMLManager.java
@@ -1,10 +1,15 @@
 package Model_Interfaces;
 
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
 public interface IXMLManager
 {
 
-  public int exportAnalysis(IRequirementAnalysis analysis, String address);
+  public int exportAnalysis(IRequirementAnalysis analysis, String address)
+    throws JAXBException, FileNotFoundException;
 
-  public IRequirementAnalysis importAnalysis();
+  public IRequirementAnalysis importAnalysis(String address);
 
 }
